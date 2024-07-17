d49419b7df87c21f6a61488decebda842b42a10c
sukibaby <163092272+sukibaby@users.noreply.github.com>
2024-07-17 00:39:51 -0700

    Update banner.lua

--- BGAnimations/ScreenSelectMusic overlay/banner.lua
+++ BGAnimations/ScreenSelectMusic overlay/banner.lua
@@ -4,13 +4,9 @@ local SongOrCourse, banner
 
 local t = Def.ActorFrame{
 	OnCommand=function(self)
-		if IsUsingWideScreen() then
-			self:zoom(0.7655)
-			self:xy(_screen.cx - 170, 96)
-		else
-			self:zoom(0.75)
-			self:xy(_screen.cx - 166, 96)
-		end
+		local zoomLevel = IsUsingWideScreen() and 0.7655 or 0.75
+		local xOffset = IsUsingWideScreen() and -170 or -166
+		self:zoom(zoomLevel):xy(_screen.cx + xOffset, 96)
 	end,
 
 	Def.ActorFrame{
@@ -18,11 +14,7 @@ local t = Def.ActorFrame{
 		CurrentCourseChangedMessageCommand=function(self) self:playcommand("Set") end,
 		SetCommand=function(self)
 			SongOrCourse = GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentCourse() or GAMESTATE:GetCurrentSong()
-			if SongOrCourse and SongOrCourse:HasBanner() then
-				self:visible(false)
-			else
-				self:visible(true)
-			end
+			self:visible(not (SongOrCourse and SongOrCourse:HasBanner()))
 		end,
 
 		LoadActor(banner_directory.."/banner"..SL.DefaultColor.." (doubleres).png" )..{
@@ -47,15 +39,13 @@ local t = Def.ActorFrame{
 	-- the MusicRate Quad and text
 	Def.ActorFrame{
 		InitCommand=function(self)
-			self:visible( SL.Global.ActiveModifiers.MusicRate ~= 1 ):y(75)
+			self:visible(SL.Global.ActiveModifiers.MusicRate ~= 1):y(75)
 		end,
 
-		--quad behind the music rate text
 		Def.Quad{
-			InitCommand=function(self) self:diffuse( color("#1E282FCC") ):zoomto(418,14) end
+			InitCommand=function(self) self:diffuse(color("#1E282FCC")):zoomto(418,14) end
 		},
 
-		--the music rate text
 		LoadFont("Common Normal")..{
 			InitCommand=function(self) self:shadowlength(1):zoom(0.85) end,
 			OnCommand=function(self)
